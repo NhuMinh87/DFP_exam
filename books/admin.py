2e2d012d55b2ddfd2967eb2e73c542e41dd3aec1
Nhu Minh <minhnguyennhu87@gmail.com>
2026-05-29 19:24:21 +0700

    Improve UI and update book model

--- books/admin.py
+++ books/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'author',
+        'price',
+        'created_at',
+    )
+
+    readonly_fields = (
+        'created_at',
+    )
